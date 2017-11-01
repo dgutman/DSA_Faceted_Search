593fae64a792d8f0a30decb24e1d7b0bdc9f5b0e
dagutman <dagutman@gmail.com>
2017-11-01 14:17:08 -0400

    updated girder utils.py

--- pythonLoadScripts/girder_utils.py
+++ pythonLoadScripts/girder_utils.py
@@ -4,101 +4,31 @@ import girder_client
 #Modified by Juan Carlos Vizcarra
 #Last updated: 10/10/2017
 
-def recurseGetItems(client, folderId, parentType='folder', printLog=False):
+def recurseGetItems(client, folderID, parentType='folder'):
     '''
-    JCV: This function was modified to handle cases when the folder id corresponds to a collection instead of a folder
-    In this case it requires an optional parameter (parentType) to be set to collection
+    Returns a list of json objects representing the items inside the folderID given in the appropriate api
+    This algorithm implements recursion method
     
     INPUTS
     client - a girder client object
     folderId - a string id of the folder or collection
     parentType - only needs to be modified if your folderId is for a collection (see above)
-    printLog - set to True to informative output to be printed (only works for collection Ids)
-    
+        
     OUTPUT
-    slideDict - when dealing with collections it returns a dictionary with the slides info in a folder structure
     items - an array containing slide info
     '''
-    
-    if parentType=='collection':
-        topLevelFolders = client.listFolder(folderId, parentFolderType=parentType)
-        slideDict = {} #Dict where the top level key is the folder name, and below are the slides in that folder
-        
-        for tlf in topLevelFolders:
-            slidesInFolder = recurseGetItems(client, tlf['_id'])
-            if printLog:
-                print "Retrieving slides in",tlf['name'],";", len(slidesInFolder),"files are in the current folder"
-            slideDict[tlf['name']] = slidesInFolder    
-        return slideDict
-    else:        
-        items = list(recurseGetResource(client, folderId, 'item'))
-        folders = recurseGetResource(client, folderId, 'folder')
-
-        for folder in folders:
-            tmp = recurseGetItems(client, folder["_id"])
-            items += list(tmp)
-
-        return items
-
-def recurseGetResource(client, parentId, resourceType, parentType='folder'):
-
-
-    ### The recursion logic is broken here...
-    """
-    Recurse below the parent(indicated by parentId) and generate a list of all
-    resources of type resourceType that existed under the parent.
-    :param parentId: Id of the collection or folder to be searched.
-    :type parentId: ObjectId
-    :param resourceType: Either 'item' or 'folder'. Indicates whether nested
-    folder data or item data should be collected.
-    :type resourceType: str
-    :param parentType: Either 'folder' or 'collection'. Indicates whether
-    the parentId is a collection id or a folder id.
-    :type parentType: str
-    :returns: A list of all folders or items below parentId.
-    :rtype: list of dict
-    """
-    # now get all folders
-    resourceList = []
-
+    folders = []
+    folders.extend(client.listFolder(folderID, parentFolderType=parentType))
+    items = []
     try:
-        folders = client.listFolder(parentId, parentFolderType=parentType)
-    except girder_client.HttpError as err:
-        print err
-#        print HttpError(err)
-        return []
-
-    #The line below is commented and moved below because this is an iterator and 
-    #it consume the list only once, you will ending extending resourceList to None
-    #folderIdList = getField(folders, '_id')
-
-    if resourceType is 'item' and parentType is not 'collection':
-        try:
-            data = client.listItem(parentId)
-            resourceList.extend(data)
-        except girder_client.HttpError as err:
-            print "HTTP Error thrown"
-
-#            print HttpError(err)
-            print err
-            return []
-    elif resourceType is 'folder':
-        resourceList.extend(folders)
-    elif resourceType is not 'item' or resourceType is not 'folder':
-        raise Exception('Invalid resourceType: %s' % resourceType)
-
-    #folderIdList is an iterator and can only be consumed once. Do not move it up in the function
-    folderIdList = getField(folders, '_id')
-
-    ### The recursion does NOT work if you start with a collection.. probably iwll work if it's a folder.
-    for folderId in folderIdList:
-        #if re
-        resourceList.extend(recurseGetResource(client, folderId, resourceType))
-
-    return resourceList
-
-def getField(data, strKey):
-    return [i[strKey] for i in data]
+        items.extend(client.listItem(folderID))
+    except:
+        items = []
+        
+    if len(folders) is not 0:
+        for fld in folders:
+            items.extend(recurseGetItems(client, fld['_id']))
+    return items
 
 
 def lookupItemByName( girderClient, parentFolderID, itemName):
@@ -199,7 +129,7 @@ def validateSlideMetaData( slideMetaData, validStainTypes ):
         return (True, [])
     
 
-def create_curated_folder(girderClient, FolderToCurate_ID, TargetFolder_ID, stain_Types, make_unknown=True):
+def create_curated_folder(girderClient, FolderToCurate_ID, TargetFolder_ID, stain_Types, make_unknown=True, ommit=[]):
     """
     A routine that takes in a starting folder in the API and a destination folder, and reorganizes the data
     to match a speficied format. In short, it creates a folder structure that is grouped by desired metadata found in the 
@@ -213,6 +143,7 @@ def create_curated_folder(girderClient, FolderToCurate_ID, TargetFolder_ID, stai
     OPTIONAL_INPUTS:
         make_unknown: default is True and results in slides that don't match the stain type given to be put in their own
             unkown subfolder. If False
+        ommit: if you know there is a folder(s) you don't want include, then pass that string name in ommit array
     OUTPUT:
         stats: dictionary with two keys. SlidesThatFailed contains the name of the slides that did not match the format. 
             Errors contains info on why the slides specifically failed
@@ -222,25 +153,126 @@ def create_curated_folder(girderClient, FolderToCurate_ID, TargetFolder_ID, stai
     allerrors = []
     
     for folder in gc.listFolder(FolderToCurate_ID): #list all folders, one level down from collection
-        #the folders also have the patient ID, so could get these via regex to deal with the unknown cases
-        patientID = folder['name'] #from folder name, NOT metadata
+        if folder['name'] not in ommit:
+            #the folders also have the patient ID, so could get these via regex to deal with the unknown cases
+            patientID = folder['name'] #from folder name, NOT metadata
         
-        curPatientData = recurseGetItems(gc,folder['_id']) #get all the slides in each folder
-        for cpd in curPatientData: #for each slide
-            if cpd['name'].endswith(('ndpi','svs')) : #these are the valid slides, so for each slide
-                ### let's figure out what metadata keys we need in order to be happy about the slide
-                try:
-                    (metaDataGood,errors) =  validateSlideMetaData( cpd['meta'] , stain_Types)
-                    if len(errors) != 0:
-                        allerrors.append(errors)
-                except:
-                    print "No metadata for", cpd['name']
-                    continue
-                if metaDataGood: #only true if it passed the above
-                    copySlideToCuratedFolder(gc, cpd, cpd['meta'], TargetFolder_ID, patientID)
-                else:
-                    if make_unknown: 
-                        copySlideToCuratedFolder(gc, cpd, None, TargetFolder_ID, patientID)
-                    SlidesThatFailed.append(cpd['name'])
+            curPatientData = recurseGetItems(gc,folder['_id']) #get all the slides in each folder
+            for cpd in curPatientData: #for each slide
+                if cpd['name'].endswith(('ndpi','svs')) : #these are the valid slides, so for each slide
+                    ### let's figure out what metadata keys we need in order to be happy about the slide
+                    try:
+                        (metaDataGood,errors) =  validateSlideMetaData( cpd['meta'] , stain_Types)
+                        if len(errors) != 0:
+                            allerrors.append(errors)
+                    except:
+                        print "No metadata for", cpd['name']
+                        continue
+                    if metaDataGood: #only true if it passed the above
+                        copySlideToCuratedFolder(gc, cpd, cpd['meta'], TargetFolder_ID, patientID)
+                    else:
+                        if make_unknown: 
+                            copySlideToCuratedFolder(gc, cpd, None, TargetFolder_ID, patientID)
+                        SlidesThatFailed.append(cpd['name'])
+        else:
+            print 'Skipping folder name:',folder['name']
     stats = dict([('SlidesThatFailed',SlidesThatFailed),('Errors', allerrors)])
     return stats
+
+'''
+def recurseGetItems(client, folderId, parentType='folder', printLog=False):
+    
+    JCV: This function was modified to handle cases when the folder id corresponds to a collection instead of a folder
+    In this case it requires an optional parameter (parentType) to be set to collection
+    
+    INPUTS
+    client - a girder client object
+    folderId - a string id of the folder or collection
+    parentType - only needs to be modified if your folderId is for a collection (see above)
+    printLog - set to True to informative output to be printed (only works for collection Ids)
+    
+    OUTPUT
+    slideDict - when dealing with collections it returns a dictionary with the slides info in a folder structure
+    items - an array containing slide info
+    
+    
+    if parentType=='collection':
+        topLevelFolders = client.listFolder(folderId, parentFolderType=parentType)
+        slideDict = {} #Dict where the top level key is the folder name, and below are the slides in that folder
+        
+        for tlf in topLevelFolders:
+            slidesInFolder = recurseGetItems(client, tlf['_id'])
+            if printLog:
+                print "Retrieving slides in",tlf['name'],";", len(slidesInFolder),"files are in the current folder"
+            slideDict[tlf['name']] = slidesInFolder    
+        return slideDict
+    else:        
+        items = list(recurseGetResource(client, folderId, 'item'))
+        folders = recurseGetResource(client, folderId, 'folder')
+
+        for folder in folders:
+            tmp = recurseGetItems(client, folder["_id"])
+            items += list(tmp)
+
+        return items
+
+def recurseGetResource(client, parentId, resourceType, parentType='folder'):
+
+
+    ### The recursion logic is broken here...
+    """
+    Recurse below the parent(indicated by parentId) and generate a list of all
+    resources of type resourceType that existed under the parent.
+    :param parentId: Id of the collection or folder to be searched.
+    :type parentId: ObjectId
+    :param resourceType: Either 'item' or 'folder'. Indicates whether nested
+    folder data or item data should be collected.
+    :type resourceType: str
+    :param parentType: Either 'folder' or 'collection'. Indicates whether
+    the parentId is a collection id or a folder id.
+    :type parentType: str
+    :returns: A list of all folders or items below parentId.
+    :rtype: list of dict
+    """
+    # now get all folders
+    resourceList = []
+
+    try:
+        folders = client.listFolder(parentId, parentFolderType=parentType)
+    except girder_client.HttpError as err:
+        print err
+#        print HttpError(err)
+        return []
+
+    #The line below is commented and moved below because this is an iterator and 
+    #it consume the list only once, you will ending extending resourceList to None
+    #folderIdList = getField(folders, '_id')
+
+    if resourceType is 'item' and parentType is not 'collection':
+        try:
+            data = client.listItem(parentId)
+            resourceList.extend(data)
+        except girder_client.HttpError as err:
+            print "HTTP Error thrown"
+
+#            print HttpError(err)
+            print err
+            return []
+    elif resourceType is 'folder':
+        resourceList.extend(folders)
+    elif resourceType is not 'item' or resourceType is not 'folder':
+        raise Exception('Invalid resourceType: %s' % resourceType)
+
+    #folderIdList is an iterator and can only be consumed once. Do not move it up in the function
+    folderIdList = getField(folders, '_id')
+
+    ### The recursion does NOT work if you start with a collection.. probably iwll work if it's a folder.
+    for folderId in folderIdList:
+        #if re
+        resourceList.extend(recurseGetResource(client, folderId, resourceType))
+
+    return resourceList
+
+def getField(data, strKey):
+    return [i[strKey] for i in data]
+'''
